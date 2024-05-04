9b2e0d8989a12798e5b1372d7962e9deca874be2
Gavriel Linoy <kraserultra@gmail.com>
2024-05-05 00:22:02 +0300

    Algorithm didn't work :(

--- War/src/main.py
+++ War/src/main.py
@@ -27,11 +27,26 @@ def get_frequencies(data: str) -> dict:
 
     return get_sorted_dict_by_values(count)
     
+def get_priority_in_dict(dct: dict, val: any) -> int:
+    index = 0
+    for key in dct.keys():
+        if key == val:
+            return index
+        index += 1
+        
+    return -1
+ 
 
 def main() -> None:
     with open('./resources/encrypted.txt', READ_FILE_MODE) as enc_file:
         data = enc_file.read()
         freq = get_frequencies(data=data)
+        
+        with open('./output.txt', WRITE_FILE_MODE) as dec_file:
+            for ch in data:
+                p = get_priority_in_dict(freq, ch)
+                if p != -1 and p < len(LETTERS_SCORE.keys()):
+                    dec_file.write(list(LETTERS_SCORE.items())[p][0])             
 
 if __name__ == "__main__":
     abspath = os.path.abspath(__file__)
